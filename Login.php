2ef915f1372ecdb358615a287027f8b69b1e2e80
BaasithBacker <bh.BasithBacker07@gmail.com>
2021-03-10 15:15:46 +0530

    New

--- Login.php
+++ Login.php
@@ -16,17 +16,17 @@
             <div class="col col-12 col-sm-12 col-md-12 col-lg-12">
               <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                  <a class="navbar-brand" href="#">B&B</a>
+                  <a class="navbar-brand" href="context.php">B&B</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                       <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="context.html">Home</a>
+                        <a class="nav-link active" aria-current="page" href="context.php">Home</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="Login.html">Login</a>
+                        <a class="nav-link active" aria-current="page" href="Login.php">Login</a>
                       </li>
                     
                 
